25ed840bc1fb0ccc6ec1399cd6857ac187f74bad
Humaira Rahaman <humairamitul@gmail.com>
2022-03-31 16:32:44 +0600

    scores

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -15,7 +15,7 @@ const CoinDetails = () => {
         } )
     },[id])
     return (
-        <div className=' px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+        <div className=' px-4 h-[80vh] pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
             <div className=' h-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 md:justify-around'>
                 <div> <h1 className=' text-3xl'>General Info:</h1>
                 <hr/>
@@ -26,6 +26,13 @@ const CoinDetails = () => {
                 <h1>Hashing Algorithm: {coin.hashing_algorithm}</h1>
                 <h1>Genesis Date: {coin.genesis_date}</h1>
                 <h1>Last Updated: {coin.last_updated}</h1>
+
+                <h1 className=' text-3xl mt-4'>Scores:</h1>
+                <hr/>
+                <h1>Community Score: {coin.community_score}</h1>
+                <h1>Developer Score: {coin.developer_score}</h1>
+                <h1>Liquidity Score: {coin.liquidity_score}</h1>
+                <h1>Public Interest Score: {coin.public_interest_score}</h1>
                  </div>
                 <div className=' flex justify-center items-center'>
                     <img src={coin.image?.large} alt="coinimg" />
